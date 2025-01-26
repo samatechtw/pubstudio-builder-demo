fdf2c8484d62fd94f5387f659a4b93da2e338c0a
Sam Pullman <sampullman@gmail.com>
2025-01-26 17:06:11 +0800

    chore: update packages and replace eslint with oxlint

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { ApiInjectionKey, StoreInjectionKey, store } from '@pubstudio/builder'
-import { createApp, h } from 'vue'
+import { createApp } from 'vue'
 import { rootApi } from './api'
 import App from './App.vue'
 import i18n from './i18n'
--- src/shims-vue.ts
+++ src/shims-vue.ts
@@ -1,10 +1,11 @@
-declare module "*.vue" {
-  import type { DefineComponent } from "vue";
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
   // eslint-disable-next-line
-  const component: DefineComponent<{}, {}, any>;
-  export default component;
+  const component: DefineComponent<{}, {}, any>
+  export default component
 }
 
+// eslint-disable-next-line
 interface ImportMeta {
-  env: Record<string, string | undefined>;
+  env: Record<string, string | undefined>
 }
